cfe1ef9cadfc3224bfa0fe9dd300ccd5bdbdfefc
Dmitry <brothersds@ngs.ru>
2023-01-10 19:44:01 +0300

    chrome_path 2

--- lesson_3.py
+++ lesson_3.py
@@ -1,5 +1,14 @@
+import time
+
 from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path='/home/dmitry/Geek/PycharmProjects/python_selenium/chromedriver')
+from selenium.webdriver.chrome.service import Service
+
+#driver = webdriver.Chrome(executable_path='/home/dmitry/Geek/PycharmProjects/python_selenium/chromedriver')
+#driver = webdriver.Firefox(executable_path='/home/dmitry/Geek/PycharmProjects/python_selenium')
+s = Service('/home/dmitry/Geek/PycharmProjects/python_selenium/chromedriver')
+driver = webdriver.Chrome(service=s)
 driver.get('https://www.saucedemo.com/')
 driver.maximize_window()
+time.sleep(10)
+driver.close()
