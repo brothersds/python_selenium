a96d53b7711b79b67005b61d6ae201986bd96a4e
Dmitry <brothersds@ngs.ru>
2023-01-23 22:53:20 +0300

    test_5 click link about

--- test_5.py
+++ test_5.py
@@ -16,16 +16,23 @@ user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")
 user_name.send_keys(login_standard_user)
 print("Input Login")
 
+# time.sleep(3)
+# user_name.clear()
+
+password = driver.find_element(By.XPATH, "//input[@id='password']")
+password.send_keys(password_all)
+print("Input Password")
+button_login = driver.find_element(By.XPATH, "//input[@id='login-button']")
+button_login.click()
+print("Click Login Button")
+
+menu = driver.find_element(By.XPATH, "//button[@id='react-burger-menu-btn']")
+menu.click()
+print("Click Menu Button")
 time.sleep(3)
-
-user_name.clear()
-
-# password = driver.find_element(By.XPATH, "//input[@id='password']")
-# password.send_keys(password_all)
-# print("Input Password")
-# button_login = driver.find_element(By.XPATH, "//input[@id='login-button']")
-# button_login.click()
-# print("Click Login Button")
+link_about = driver.find_element(By.XPATH, "//a[@id='about_sidebar_link']")
+link_about.click()
+print("Click Link Button")
 
 
 time.sleep(1)
