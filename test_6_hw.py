0a1a6b947a17e867ccd7c328660e502cd4ccca6e
Dmitry <brothersds@ngs.ru>
2023-01-26 21:32:01 +0300

    test_6_hw smoke test two products in cart

---
+++ test_6_hw.py
@@ -0,0 +1,147 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(executable_path='/home/dmitry/Geek/PycharmProjects/python_selenium/chromedriver')
+base_url = 'https://www.saucedemo.com/'
+driver.get(base_url)
+driver.maximize_window()
+
+login_standard_user = "standard_user"
+password_all = "secret_sauce"
+
+user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")
+user_name.send_keys(login_standard_user)
+print("Input Login")
+password = driver.find_element(By.XPATH, "//input[@id='password']")
+password.send_keys(password_all)
+print("Input Password")
+button_login = driver.find_element(By.XPATH, "//input[@id='login-button']")
+button_login.click()
+print("Click Login Button")
+
+"""INFO Product 1"""
+product_1 = driver.find_element(By.XPATH, "//a[@id='item_4_title_link']")
+value_product_1 = product_1.text
+print(value_product_1)
+
+price_product_1 = driver.find_element(By.XPATH, "//*[@id='inventory_container']/div/div[1]/div[2]/div[2]/div")
+value_price_product_1 = price_product_1.text
+print(value_price_product_1)
+
+select_product_1 = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-backpack']")
+select_product_1.click()
+print("Select Product 1")
+
+"""INFO Product 2"""
+product_2 = driver.find_element(By.XPATH, "//a[@id='item_2_title_link']")
+value_product_2 = product_2.text
+print(value_product_2)
+
+price_product_2 = driver.find_element(By.XPATH, "//*[@id='inventory_container']/div/div[5]/div[2]/div[2]/div")
+value_price_product_2 = price_product_2.text
+print(value_price_product_2)
+
+select_product_2 = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-onesie']")
+select_product_2.click()
+print("Select Product 2")
+
+"""Click Cart"""
+cart = driver.find_element(By.XPATH, "//span[@class='shopping_cart_badge']")
+cart.click()
+print("Enter Cart")
+
+"""INFO Cart Product 1"""
+cart_product_1 = driver.find_element(By.XPATH, "//a[@id='item_4_title_link']")
+value_cart_product_1 = cart_product_1.text
+print(value_cart_product_1)
+assert value_product_1 == value_cart_product_1
+print("INFO Cart Product 1 GOOD")
+
+price_cart_product_1 = driver.find_element(By.XPATH, "//*[@id='cart_contents_container']\
+                                                    /div/div[1]/div[3]/div[2]/div[2]/div")
+value_price_cart_product_1 = price_cart_product_1.text
+print(value_price_cart_product_1)
+assert value_price_product_1 == value_price_cart_product_1
+print("INFO Price Cart Product 1 GOOD")
+
+"""INFO Cart Product 2"""
+cart_product_2 = driver.find_element(By.XPATH, "//a[@id='item_2_title_link']")
+value_cart_product_2 = cart_product_2.text
+print(value_cart_product_2)
+assert value_product_2 == value_cart_product_2
+print("INFO Cart Product 2 GOOD")
+
+price_cart_product_2 = driver.find_element(By.XPATH, "//*[@id='cart_contents_container']\
+                                                     /div/div[1]/div[4]/div[2]/div[2]/div")
+value_price_cart_product_2 = price_cart_product_2.text
+print(value_price_cart_product_2)
+assert value_price_product_2 == value_price_cart_product_2
+print("INFO Price Cart Product 2 GOOD")
+
+"""Click Checkout"""
+checkout = driver.find_element(By.XPATH, "//button[@id='checkout']")
+checkout.click()
+print("Click Checkout")
+
+"""Select User INFO"""
+first_name = driver.find_element(By.XPATH, "//input[@id='first-name']")
+first_name.send_keys("Don")
+print("Input First Name")
+
+last_name = driver.find_element(By.XPATH, "//input[@id='last-name']")
+last_name.send_keys("Capone")
+print("Input Last Name")
+
+postal_code = driver.find_element(By.XPATH, "//input[@id='postal-code']")
+postal_code.send_keys("12345")
+print("Input Postal Code")
+
+button_continue = driver.find_element(By.XPATH, "//input[@id='continue']")
+button_continue.click()
+print("Click Continue")
+
+"""INFO Finish Product 1"""
+finish_product_1 = driver.find_element(By.XPATH, "//a[@id='item_4_title_link']")
+value_finish_product_1 = finish_product_1.text
+print(value_finish_product_1)
+assert value_product_1 == value_finish_product_1
+print("INFO Finish Product 1 GOOD")
+
+price_finish_product_1 = driver.find_element(By.XPATH, "//*[@id='checkout_summary_container']\
+                                                          /div/div[1]/div[3]/div[2]/div[2]/div")
+value_price_finish_product_1 = price_finish_product_1.text
+print(value_price_finish_product_1)
+assert value_price_product_1 == value_price_finish_product_1
+print("INFO Price Finish Product 1 GOOD")
+
+"""INFO Finish Product 2"""
+finish_product_2 = driver.find_element(By.XPATH, "//a[@id='item_2_title_link']")
+value_finish_product_2 = finish_product_2.text
+print(value_finish_product_2)
+assert value_product_2 == value_finish_product_2
+print("INFO Finish Product 2 GOOD")
+
+price_finish_product_2 = driver.find_element(By.XPATH, "//*[@id='checkout_summary_container']\
+                                                        /div/div[1]/div[4]/div[2]/div[2]/div")
+value_price_finish_product_2 = price_finish_product_2.text
+print(value_price_finish_product_2)
+assert value_price_product_2 == value_price_finish_product_2
+print("INFO Price Finish Product 2 GOOD")
+
+"""Total Summary Price"""
+summary_price = driver.find_element(By.XPATH, "//*[@id='checkout_summary_container']/div/div[2]/div[5]")
+value_summary_price = summary_price.text
+print(value_summary_price)
+new_value_price_finish_product_1 = float(value_price_finish_product_1[1:])
+new_value_price_finish_product_2 = float(value_price_finish_product_2[1:])
+result = new_value_price_finish_product_1 + new_value_price_finish_product_2
+item_total = f'Item total: ${result}'
+print(item_total)
+assert item_total == value_summary_price
+print("Total summary Price GOOD")
+
+
+time.sleep(2)
+driver.close()
