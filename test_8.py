bd093c0c674193e7e9dabb379c455fa247129d6e
Dmitry <brothersds@ngs.ru>
2023-01-31 19:59:36 +0300

    test_8_2 select radio button

--- test_8.py
+++ test_8.py
@@ -1,20 +1,23 @@
-import datetime
 import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(executable_path='/home/dmitry/Geek/PycharmProjects/python_selenium/chromedriver')
-base_url = 'https://demoqa.com/checkbox'
+# base_url = 'https://demoqa.com/checkbox'
+base_url = 'https://demoqa.com/radio-button'
 driver.get(base_url)
 driver.maximize_window()
 
 # """Select Check Box"""
 # check_box = driver.find_element(By.XPATH, "//span[@class='rct-checkbox']")
 # check_box.click()
-"""Select Menu Check Box"""
-check_box = driver.find_element(By.XPATH, "//button[@aria-label='Toggle']")
-check_box.click()
+# """Select Menu Check Box"""
+# check_box = driver.find_element(By.XPATH, "//button[@aria-label='Toggle']")
+# check_box.click()
+"""Select Menu Radio Button"""
+radio_button = driver.find_element(By.XPATH, "//label[@for='yesRadio']")
+radio_button.click()
 
 time.sleep(2)
 driver.close()
