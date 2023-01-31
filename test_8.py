bde4d6658f66d15029570c19d1c67415629ffca4
Dmitry <brothersds@ngs.ru>
2023-01-31 20:27:34 +0300

    test_8_3 select Double and Right Click, ActionChains

--- test_8.py
+++ test_8.py
@@ -1,11 +1,13 @@
 import time
 
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(executable_path='/home/dmitry/Geek/PycharmProjects/python_selenium/chromedriver')
 # base_url = 'https://demoqa.com/checkbox'
-base_url = 'https://demoqa.com/radio-button'
+# base_url = 'https://demoqa.com/radio-button'
+base_url = 'https://demoqa.com/buttons'
 driver.get(base_url)
 driver.maximize_window()
 
@@ -15,9 +17,16 @@ driver.maximize_window()
 # """Select Menu Check Box"""
 # check_box = driver.find_element(By.XPATH, "//button[@aria-label='Toggle']")
 # check_box.click()
-"""Select Menu Radio Button"""
-radio_button = driver.find_element(By.XPATH, "//label[@for='yesRadio']")
-radio_button.click()
+# """Select Menu Radio Button"""
+# radio_button = driver.find_element(By.XPATH, "//label[@for='yesRadio']")
+# radio_button.click()
+"""Select Double and Right Click"""
+action = ActionChains(driver)
+double = driver.find_element(By.XPATH, "//button[@id='doubleClickBtn']")
+action.double_click(double).perform()
 
-time.sleep(2)
+right_click = driver.find_element(By.XPATH, "//button[@id='rightClickBtn']")
+action.context_click(right_click).perform()
+
+time.sleep(3)
 driver.close()
