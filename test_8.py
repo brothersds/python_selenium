870c23f8b9f1092b566d25f3b90d02eb8469a4e5
Dmitry <brothersds@ngs.ru>
2023-01-31 22:26:02 +0300

    test_9 exception (try except)

--- test_8.py
+++ test_8.py
@@ -2,6 +2,7 @@ import datetime
 import time
 
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver import ActionChains, Keys
 from selenium.webdriver.common.by import By
 
@@ -9,7 +10,9 @@ driver = webdriver.Chrome(executable_path='/home/dmitry/Geek/PycharmProjects/pyt
 # base_url = 'https://demoqa.com/checkbox'
 # base_url = 'https://demoqa.com/radio-button'
 # base_url = 'https://demoqa.com/buttons'
-base_url = 'https://demoqa.com/date-picker'
+# base_url = 'https://demoqa.com/date-picker'
+# base_url = 'https://demoqa.com/dynamic-properties'
+base_url = 'https://demoqa.com/radio-button'
 driver.get(base_url)
 driver.maximize_window()
 
@@ -30,7 +33,7 @@ driver.maximize_window()
 # right_click = driver.find_element(By.XPATH, "//button[@id='rightClickBtn']")
 # action.context_click(right_click).perform()
 
-new_date = driver.find_element(By.XPATH, "//input[@id='datePickerMonthYearInput']")
+# new_date = driver.find_element(By.XPATH, "//input[@id='datePickerMonthYearInput']")
 # new_date.clear() #Не все стирает, оставляет символы
 # new_date.send_keys(Keys.BACKSPACE)
 # new_date.send_keys(Keys.BACKSPACE)
@@ -44,17 +47,46 @@ new_date = driver.find_element(By.XPATH, "//input[@id='datePickerMonthYearInput'
 # new_date.send_keys(Keys.BACKSPACE)
 # time.sleep(2)
 # new_date.send_keys("06/17/2022")
-new_date.click()
-time.sleep(2)
-now_date = datetime.datetime.utcnow().strftime("%d")
-print(now_date)
-date = int(now_date) - 1
-locator = f"//div[@aria-label='Choose Monday, January {date}th, 2023']"
-print(locator)
+# new_date.click()
+# time.sleep(2)
+# now_date = datetime.datetime.utcnow().strftime("%d")
+# print(now_date)
+# date = int(now_date) - 1
+# locator = f"//div[@aria-label='Choose Monday, January {date}th, 2023']"
+# print(locator)
 
-date_17 = driver.find_element(By.XPATH, locator)
+# date_30 = driver.find_element(By.XPATH, locator)
 # date_17 = driver.find_element(By.XPATH, "//div[contains(@class,'react-datepicker__day--today')]")
-date_17.click()
+# date_30.click()
+
+#"""Отработка исключений"""
+# try:
+#     visible_button = driver.find_element(By.XPATH, "//button[@id='visibleAfter']")
+#     visible_button.click()
+# except NoSuchElementException as exception:
+#     print("NoSuchElementException exception")
+#     time.sleep(5)
+#     visible_button = driver.find_element(By.XPATH, "//button[@id='visibleAfter']")
+#     visible_button.click()
+#     print("Click Visible Button")
+yes_radio = driver.find_element(By.XPATH, "//label[@for='yesRadio']")
+yes_radio.click()
+try:
+    message = driver.find_element(By.XPATH, "//span[@class='text-success']")
+    value_message = message.text
+    print(value_message)
+    assert value_message == "No"
+except AssertionError as exception:
+    driver.refresh()
+    yes_radio = driver.find_element(By.XPATH, "//label[@for='yesRadio']")
+    yes_radio.click()
+    message = driver.find_element(By.XPATH, "//span[@class='text-success']")
+    value_message = message.text
+    print(value_message)
+    assert value_message == "Yes"
+    print("Radio Button YES")
+print("Test Over")
+
 
 time.sleep(3)
 driver.close()
